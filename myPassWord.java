ef813564cf255c297d329ed33bed3f12f5d8065e
Versprechen080 <109969279+Versprechen080@users.noreply.github.com>
2022-10-01 22:56:54 +1000

    Create myPassWord.java

---
+++ myPassWord.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+import java.util.Scanner;
+
+public class myPassWord {
+    private  static void placeComponents(JPanel panel)
+    {
+        panel.setLayout(null);
+        //创建JLabel
+        JLabel userLabel=new JLabel("输入密码所需的位数");
+        userLabel.setBounds(120,20,165,12);
+        panel.add(userLabel);
+
+        //创建文本域用于用户输入所需密码的位数
+        JTextField userText=new JTextField(20);
+        userText.setBounds(140,40,60,30);
+        panel.add(userText);
+
+        //创建文本域用于显示随机生成的密码
+        JTextField code=new JTextField(20);
+        code.setBounds(120,80,120,30);
+        panel.add(code);
+
+        //创建生成随机密码按钮
+        JButton createCode=new JButton("生成随机密码");
+        //createCode.setBounds(120,120,120,30);
+        createCode.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // Scanner sc=new Scanner(System.in);
+                //int n=sc.nextInt();
+                int num=Integer.parseInt(code.getText());
+               // System.out.println("num=" + num);
+                String ffcode=createCode(8);
+                code.setText(ffcode);
+                //code.setBackground(Color.cyan);
+            }
+        });
+        panel.add(createCode);
+
+
+
+    }
+
+    //随机生成密码
+    public static String createCode(int n)
+    {
+        String code=" ";
+        Random r=new Random();
+        for(int i=0;i<n;i++)
+        {
+            int type=r.nextInt(3);
+            switch(type)
+            {
+                case 0:
+                    char ch=(char)(r.nextInt(26)+65);
+                    code+=ch;
+                    break;
+                case 1:
+                    char ch1=(char)(r.nextInt(26)+97);
+                    code+=ch1;
+                    break;
+                case 2:
+                    code+=r.nextInt(10);
+                    break;
+
+            }
+        }
+        return code;
+    }
+
+    public static void main(String[] args)
+    {
+        //创建JFrame实例
+        JFrame frame=new JFrame("iPassword Generator");
+        frame.setSize(350,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //创建面板
+        JPanel panel=new JPanel();
+
+        //添加面板
+        frame.add(panel);
+
+        //调用用户定义的方法并添加组件到面板
+        placeComponents(panel);
+
+        //设置界面可见
+        frame.setVisible(true);
+
+
+    }
+}
